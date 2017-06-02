9d2e2a050af0cc4bec78594ae4545e9211803d8b
nova <nova@nova01.home>
2017-06-02 18:41:00 +0200

    Working on HIB version

---
+++ scripts/novaathib.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+while true
+do
+sudo java -Xmx1024m -Djava.library.path=../lib -cp ../bin:../lib/jnetpcap.jar:../lib/mail.jar ch.digisyn.nova.NOVAControl ../hib_10x2.properties
+sleep 1
+done
