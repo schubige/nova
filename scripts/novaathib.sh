d8fb49c4e7f34e810988dd3069ed5f4a7a7b1692
Simon Schubiger <simon.schubiger@fhnw.ch>
2017-09-18 11:01:01 +0200

    added renice to startup script

--- scripts/novaathib.sh
+++ scripts/novaathib.sh
@@ -3,5 +3,7 @@ while true
 do
 ./compile.sh
 sudo java -Xmx1024m -Djava.library.path=../lib -cp ../bin:../lib/jnetpcap.jar:../lib/mail.jar ch.digisyn.nova.NOVAControl ../hib_10x2.properties
+sudo renice -19 $(pidof java)
+sudo renice 19 $(pidof TeamViewer_Desktop)
 sleep 1
 done
