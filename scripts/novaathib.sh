ad61669a31d67bac2b9802e6c51713be5e4d22a1
Simon Schubiger <simon.schubiger@fhnw.ch>
2017-09-18 11:10:36 +0200

    starting java with nice

--- scripts/novaathib.sh
+++ scripts/novaathib.sh
@@ -2,8 +2,7 @@
 while true
 do
 ./compile.sh
-sudo java -Xmx1024m -Djava.library.path=../lib -cp ../bin:../lib/jnetpcap.jar:../lib/mail.jar ch.digisyn.nova.NOVAControl ../hib_10x2.properties > /dev/null 2>&1
-sudo renice -19 $(pidof java)
 sudo renice 19 $(pidof TeamViewer_Desktop)
+sudo nice -19 java -Xmx1024m -Djava.library.path=../lib -cp ../bin:../lib/jnetpcap.jar:../lib/mail.jar ch.digisyn.nova.NOVAControl ../hib_10x2.properties > /dev/null 2>&1
 sleep 1
 done
