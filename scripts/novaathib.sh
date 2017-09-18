b9b1d405eaa0c1dcc1165d2154d527c3603fe265
Simon Schubiger <simon.schubiger@fhnw.ch>
2017-09-18 11:17:07 +0200

    moved teamviewer renice to rc.local

--- scripts/novaathib.sh
+++ scripts/novaathib.sh
@@ -2,7 +2,6 @@
 while true
 do
 ./compile.sh
-sudo renice 19 $(pidof TeamViewer_Desktop)
-sudo nice -19 java -Xmx1024m -Djava.library.path=../lib -cp ../bin:../lib/jnetpcap.jar:../lib/mail.jar ch.digisyn.nova.NOVAControl ../hib_10x2.properties > /dev/null 2>&1
+sudo nice -n -19 java -Xmx1024m -Djava.library.path=../lib -cp ../bin:../lib/jnetpcap.jar:../lib/mail.jar ch.digisyn.nova.NOVAControl ../hib_10x2.properties > /dev/null 2>&1
 sleep 1
 done
