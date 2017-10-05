f1932b5290845d3cdc1d9355e0308bf75876491f
Simon Schubiger <simon.schubiger@fhnw.ch>
2017-10-05 16:34:35 +0200

    redirected stderr as well

--- scripts/novaathib.sh
+++ scripts/novaathib.sh
@@ -2,6 +2,6 @@
 while true
 do
 ./compile.sh
-sudo nice -n -19 java -Xmx1024m -Djava.library.path=../lib -cp ../bin:../lib/jnetpcap.jar:../lib/mail.jar ch.digisyn.nova.NOVAControl ../hib_10x2.properties > logs/latest.log
+sudo nice -n -19 java -Xmx1024m -Djava.library.path=../lib -cp ../bin:../lib/jnetpcap.jar:../lib/mail.jar ch.digisyn.nova.NOVAControl ../hib_10x2.properties > logs/latest.log 2>&1
 sleep 1
 done
