e7a42ed951c30006fd16ecbc4d52a84942e2d8d0
Simon Schubiger <simon.schubiger@fhnw.ch>
2017-10-05 16:32:30 +0200

    redirected output to log file

--- scripts/novaathib.sh
+++ scripts/novaathib.sh
@@ -2,6 +2,6 @@
 while true
 do
 ./compile.sh
-sudo nice -n -19 java -Xmx1024m -Djava.library.path=../lib -cp ../bin:../lib/jnetpcap.jar:../lib/mail.jar ch.digisyn.nova.NOVAControl ../hib_10x2.properties > /dev/null 2>&1
+sudo nice -n -19 java -Xmx1024m -Djava.library.path=../lib -cp ../bin:../lib/jnetpcap.jar:../lib/mail.jar ch.digisyn.nova.NOVAControl ../hib_10x2.properties > logs/latest.log
 sleep 1
 done
