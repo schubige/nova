c2b3ced67fe0a50270e1e4a386a486b953bcf8ce
sschubiger <sschubiger@s2b.home>
2023-12-09 14:01:24 +0100

    cleanup

--- src/ch/bluecc/nova/content/ColorSplash.java
+++ src/ch/bluecc/nova/content/ColorSplash.java
@@ -3,29 +3,29 @@ package ch.bluecc.nova.content;
 import java.util.Arrays;
 
 public class ColorSplash extends Content {
-	static final int       N_BALLS      = 5;
-	float[]                balls        = new float[N_BALLS * 9];
+	static final int       N_SPHERES    = 5;
+	float[]                spheres      = new float[N_SPHERES * 9];
 	final java.util.Random rnd          = new java.util.Random();
 	final static double    PI2          = Math.PI * 2.0;
 	
-	private void sphere(final float[] rgbFrame, final int ballIdx) {
-		final float r    = balls[ballIdx*9+3];
-		final int   minX = Math.max(0,    (int) Math.floor(balls[ballIdx*9+0]-r));
-		final int   maxX = Math.min(dimI, (int) Math.ceil (balls[ballIdx*9+0]+r));
-		final int   minY = Math.max(0,    (int) Math.floor(balls[ballIdx*9+1]-r));
-		final int   maxY = Math.min(dimJ, (int) Math.ceil (balls[ballIdx*9+1]+r));
+	private void sphere(final float[] rgbFrame, final int sphereIdx) {
+		final float r    = spheres[sphereIdx*9+3];
+		final int   minX = Math.max(0,    (int) Math.floor(spheres[sphereIdx*9+0]-r));
+		final int   maxX = Math.min(dimI, (int) Math.ceil (spheres[sphereIdx*9+0]+r));
+		final int   minY = Math.max(0,    (int) Math.floor(spheres[sphereIdx*9+1]-r));
+		final int   maxY = Math.min(dimJ, (int) Math.ceil (spheres[sphereIdx*9+1]+r));
 		final float r2   = r * r;
 		
 		for(int x = minX; x < maxX; x++) {
-			final float dx = x - balls[ballIdx*9+0];
+			final float dx = x - spheres[sphereIdx*9+0];
 			for(int y = minY; y < maxY; y++) {
-				final float dy = y - balls[ballIdx*9+1];
+				final float dy = y - spheres[sphereIdx*9+1];
 				for(int z = 0; z < dimK; z++) {
-					final float dz = z - balls[ballIdx*9+2];
+					final float dz = z - spheres[sphereIdx*9+2];
 					final float d = dx * dx + dy * dy + dz * dz;
 					final float b = (r2- d) / r2;
 					if(b > 0f)
-						addVoxelClamp(rgbFrame, x, y, z, b * balls[ballIdx*9+6], b * balls[ballIdx*9+7], b * b * balls[ballIdx*9+8]);						
+						addVoxelClamp(rgbFrame, x, y, z, b * spheres[sphereIdx*9+6], b * spheres[sphereIdx*9+7], b * b * spheres[sphereIdx*9+8]);						
 				}	
 			}			
 		}
@@ -34,34 +34,34 @@ public class ColorSplash extends Content {
 	public ColorSplash(int dimI, int dimJ, int dimK, int numFrames) {
 		super("ColorSplash", dimI, dimJ, dimK, numFrames); //$NON-NLS-1$
 		
-		for(int i = 0; i < N_BALLS; i++)
-			initBall(i, true);
+		for(int i = 0; i < N_SPHERES; i++)
+			initSphere(i, true);
 	}
 
-	private void initBall(int ballIdx, boolean firstTime) {
-		balls[ballIdx*9+0] = rnd.nextFloat() * dimI;       // X
-		balls[ballIdx*9+1] = rnd.nextFloat() * dimJ;       // Y
-		balls[ballIdx*9+2] = rnd.nextFloat() * dimK;       // Z
-		balls[ballIdx*9+3] = 0;                            // R
+	private void initSphere(int sphereIdx, boolean firstTime) {
+		spheres[sphereIdx*9+0] = rnd.nextFloat() * dimI;       // X
+		spheres[sphereIdx*9+1] = rnd.nextFloat() * dimJ;       // Y
+		spheres[sphereIdx*9+2] = rnd.nextFloat() * dimK;       // Z
+		spheres[sphereIdx*9+3] = 0;                            // R
 		if(firstTime)
-			balls[ballIdx*9+4] = rnd.nextFloat() * (float)PI2; // time offset
-		balls[ballIdx*9+5] = rnd.nextFloat() + 0.1f;       // R max
-		setRGBfromHSV(rnd.nextFloat(), 1f, 1f, balls, ballIdx * 9 + 6);
+			spheres[sphereIdx*9+4] = rnd.nextFloat() * (float)PI2; // time offset
+		spheres[sphereIdx*9+5] = rnd.nextFloat() + 0.1f;       // R max
+		setRGBfromHSV(rnd.nextFloat(), 1f, 1f, spheres, sphereIdx * 9 + 6);
 	}
 
 	@Override
 	public boolean fillFrame(float[] rgbFrame, double timeInSec) {
 		Arrays.fill(rgbFrame, 0f);
 		
-		for(int ballIdx = 0; ballIdx < N_BALLS; ballIdx++) {
-			final double  t = timeInSec + balls[ballIdx*9+4];
+		for(int ballIdx = 0; ballIdx < N_SPHERES; ballIdx++) {
+			final double  t = timeInSec + spheres[ballIdx*9+4];
 			// final float   w = (float)(dimK * (Math.sin(t)/(t%PI2)));
 			final float   w = (float)(dimK * Math.sin(t));
-			balls[ballIdx*9+3] = balls[ballIdx*9+5] * w;
-			if(balls[ballIdx*9+3] > 0)
+			spheres[ballIdx*9+3] = spheres[ballIdx*9+5] * w;
+			if(spheres[ballIdx*9+3] > 0)
 				sphere(rgbFrame, ballIdx);
-			else if(balls[ballIdx*9+3] < 0)
-				initBall(ballIdx, false);
+			else if(spheres[ballIdx*9+3] < 0)
+				initSphere(ballIdx, false);
 		}
 		return --frames > 0;
 	}
--- src/ch/bluecc/nova/content/Snake.java
+++ src/ch/bluecc/nova/content/Snake.java
@@ -28,8 +28,10 @@ public class Snake extends Content {
 	private boolean isFree(int i, int j, int k) {
 		return volume[i+1][j+1][k+1] < 0.2f;
 	}
-
+	
 	class Snake_ {
+		static final int DIE_FRAMES = 50;
+		
 		int[][] trace = new int[30][3];
 		
 		int i;
@@ -46,6 +48,7 @@ public class Snake extends Content {
 
 		double  lastTime;
 		double  phase;
+		int     dying;
 		
 		Snake_() {
 			init();
@@ -79,7 +82,7 @@ public class Snake extends Content {
 				updateDirection();
 			}
 			
-			init();
+			dying = DIE_FRAMES;
 		}
 
 		private void claim(int i, int j, int k) {
@@ -103,11 +106,15 @@ public class Snake extends Content {
 			phase = rnd.nextDouble();
 		}
 
-		void run(float[] rgbFrame, double timeInSec) {
-			if(timeInSec > lastTime + 0.5 + phase) {
+		void run(float[] rgbFrame, double timeInSec) {			
+			timeInSec += phase;
+			
+			if(timeInSec > lastTime + 0.5) {
 				lastTime = timeInSec;
 				
-				move();
+				if(dying == 0)
+					move();
+				
 				for(int[] p : trace)
 					volume[p[0]][p[1]][p[2]] *= 0.95f;
 			}
@@ -115,8 +122,35 @@ public class Snake extends Content {
 				final float w = volume[p[0]][p[1]][p[2]];
 				if(p[0] > 0 && p[1] > 0 && p[2] > 0)
 					addVoxelClamp(rgbFrame, p[0]-1, p[1]-1, p[2]-1, w*rgb[0], w*rgb[1], w*rgb[2]);
-			}			
+			}
+			
+			if(dying > 0) {
+				final float r    = (float)Math.sin((Math.PI * dying) / DIE_FRAMES) * 3.5f;
+				final int   minX = Math.max(0,    (int) Math.floor(i-r));
+				final int   maxX = Math.min(dimI, (int) Math.ceil (i+r));
+				final int   minY = Math.max(0,    (int) Math.floor(j-r));
+				final int   maxY = Math.min(dimJ, (int) Math.ceil (j+r));
+				final float r2   = r * r;
+				
+				for(int x = minX; x < maxX; x++) {
+					final float dx = x - i;
+					for(int y = minY; y < maxY; y++) {
+						final float dy = y - j;
+						for(int z = 0; z < dimK; z++) {
+							final float dz = z - k;
+							final float d = dx * dx + dy * dy + dz * dz;
+							final float b = (r2- d) / r2;
+							if(b > 0f)
+								addVoxelClamp(rgbFrame, x, y, z, b * rgb[0], b * rgb[1], b * b * rgb[2]);						
+						}	
+					}			
+				}	
+				dying--;
+				if(dying == 0)
+					init();
+			}
 		}
+		
 	}
 	
 	@Override
